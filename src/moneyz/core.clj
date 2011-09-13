1c349260391c7f10550965a7bd81c4562840c421
Stathis Sideris <sideris@gmail.com>
2011-09-13 19:03:45 +0100

    First commit

---
+++ src/moneyz/core.clj
@@ -0,0 +1,43 @@
+(ns moneyz.core
+  (:use (incanter core charts io))
+  (:import (java.text SimpleDateFormat)))
+
+#_(defn read-data
+  "Use incanter's read-dataset instead."
+  [filename]
+  (let [reader (CSVReader. (get-input-reader filename) \, \" 0)
+        data (doall (map seq (seq (.readAll reader))))
+        columns (first data)
+        rows (rest data)]
+    (incanter.core.Dataset. (into [] columns) rows)))
+
+(defn parse-date [date] (.parse (SimpleDateFormat. "dd/MM/yyyy") date))
+(defn parse-dates [dates] (map parse-date dates))
+(defn to-millis [dates] (map #(.getTime %) dates))
+(def process-dates (comp to-millis parse-dates))
+
+(def d1
+     (read-dataset
+      "/Users/sideris/devel/moneyz/data/2011-08-31-statement export.csv"
+      :header true))
+
+(def d2
+     (read-dataset
+      "/Users/sideris/devel/moneyz/data/2011-09-05-statement export.csv"
+      :header true))
+
+(defn merge-statements
+  [d1 d2]
+  (dataset
+   (:column-names d1)
+   (sort-by :Date (distinct (concat (:rows d1) (:rows d2))))))
+
+(defn group-by-account [statement]
+  (into {}
+        (map
+         (fn [[key d]] [key (dataset (:column-names statement) d)])
+         (group-by :Account (:rows statement)))))
+
+(def d (group-by-account (merge-statements d1 d2)))
+
+;;(view (time-series-plot (process-dates ($ :Date d)) ($ :Amount d)))
